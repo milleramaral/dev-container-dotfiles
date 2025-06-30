7b51e52e7348799eb1f8ca64941b795fba863849
Miller Amaral <miller.miac@gmail.com>
2025-06-30 12:45:33 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # dotfiles-example
+
+## 📦 Sobre o Projeto
+
+Este repositório é um exemplo de configuração de **dotfiles** para ambientes de desenvolvimento, especialmente útil com **Dev Containers** no VS Code.
+
+## ⚙️ Configurando o Zsh no DevContainer
+
+Para utilizar o **Zsh** como shell padrão dentro do DevContainer, adicione a seguinte configuração ao seu arquivo `devcontainer.json`:
+
+```json
+{
+  "customizations": {
+    "vscode": {
+      "extensions": [],
+      "settings": {
+        "terminal.integrated.defaultProfile.linux": "zsh",
+        "terminal.integrated.profiles.linux": {
+          "bash": {
+            "path": "bash",
+            "icon": "terminal-bash"
+          },
+          "zsh": {
+            "path": "zsh"
+          },
+          "fish": {
+            "path": "fish"
+          },
+          "tmux": {
+            "path": "tmux",
+            "icon": "terminal-tmux"
+          },
+          "pwsh": {
+            "path": "pwsh",
+            "icon": "terminal-powershell"
+          }
+        }
+      }
+    }
+  }
+}
+```
+
+## Referências
+
+- https://medium.com/@jamiekt/vscode-devcontainer-with-zsh-oh-my-zsh-and-agnoster-theme-8adf884ad9f6
+- https://github.com/jamiekt/dotfiles/blob/main/install.sh
+- https://github.com/solnic/dotfiles-example/tree/main
+- https://www.youtube.com/watch?v=hp4GRij3RuA
--- install.sh
+++ install.sh
@@ -1,21 +1,56 @@
 #!/bin/bash
-
-# Add oh-my-zsh as a submodule if it's not there yet
-if [ ! -d home/oh-my-zsh ]; then
-  git submodule add https://github.com/ohmyzsh/ohmyzsh home/oh-my-zsh
-fi
-
-# Get the latest oh-my-zsh
-git submodule init
-git submodule update --recursive --remote
+## Based on the repository:
+## - https://github.com/jamiekt/dotfiles/blob/main/install.sh
+## - https://github.com/solnic/dotfiles-example/tree/main
 
 root="$(pwd)"
+echo "Installing dotfiles from $root"
 
-for file in $(ls home)
-do
-  source="$root/home/$file"
-  target="$HOME/.$file"
+echo
+echo
+echo '-----------------------------------------------------'
+echo "Copying aliases to ~/.aliases..."
+echo '-----------------------------------------------------'
+cp "$root/home/aliases" ~/.aliases
 
-  echo "Copying $source => $target"
-  cp -r $source $target
-done
+echo
+echo
+echo '-----------------------------------------------------'
+echo "Installing powerline fonts..."
+echo '-----------------------------------------------------'
+git clone --depth=1 https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
+
+# oh-my-zsh & plugins
+# wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+
+echo
+echo
+echo '-----------------------------------------------------'
+echo "Installing zsh-autosuggestions..."
+echo '-----------------------------------------------------'
+zsh -c 'git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+echo
+echo
+echo '-----------------------------------------------------'
+echo "Installing zsh-syntax-highlighting..."
+echo '-----------------------------------------------------'
+zsh -c 'git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+echo
+echo
+echo '-----------------------------------------------------'
+echo "Installing powerlevel10k..."
+echo '-----------------------------------------------------'
+zsh -c 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh}/themes/powerlevel10k'
+cp "$root/home/p10k.zsh" ~/.p10k.zsh
+
+echo
+echo
+echo '-----------------------------------------------------'
+echo "Copying zshrc to ~/.zshrc..."
+echo '-----------------------------------------------------'
+cp "$root/home/zshrc" ~/.zshrc
+echo
+echo
